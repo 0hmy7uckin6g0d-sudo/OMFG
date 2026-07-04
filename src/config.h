4011a7bb1a5dab18a864c3e4b6d5bc207d5ae061
0hmy7uckin6g0d-sudo <0hmy7uckin6g0d@gmail.com>
2026-07-03 21:15:36 -0700

    Create config.h

---
+++ src/config.h
@@ -0,0 +1,22 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define CYD_SCREEN_W 320
+#define CYD_SCREEN_H 240
+
+#define CYD_LCD_CS    15
+#define CYD_LCD_DC    2
+#define CYD_LCD_RST   4
+#define CYD_LCD_MOSI  23
+#define CYD_LCD_SCLK  18
+#define CYD_LCD_MISO  19
+
+#define CYD_BTN_UP    32
+#define CYD_BTN_DOWN  33
+#define CYD_BTN_OK    27
+#define CYD_BTN_LEFT  25
+#define CYD_BTN_RIGHT 26
+
+#define CYD_IR_PIN    17
+
+#endif
